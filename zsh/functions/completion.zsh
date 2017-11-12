5cce4ae72d60bcf39a39a126ceb5812024a85b2d
Anikin Artyom <anikinartyom@gmail.com>
2017-11-12 23:18:47 +0500

    Initialize function for zsh-completion

---
+++ zsh/functions/completion.zsh
@@ -0,0 +1,7 @@
+fpath=(~/.zsh/completion $fpath)
+
+autoload -U compinit && compinit
+
+## case-insensitive (all), partial-word and then substring completion
+zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' \
+    'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
