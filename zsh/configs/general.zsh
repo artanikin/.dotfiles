a0f8803622521b2bea109705bc485b611bc0780e
Anikin Artyom <anikinartyom@gmail.com>
2017-11-13 00:09:16 +0500

    Move setting to general.zsh

---
+++ zsh/configs/general.zsh
@@ -0,0 +1,11 @@
+export TERM=xterm-256color
+export LANG=en_US.UTF-8
+export EDITOR=nvim
+export GREP_OPTIONS='--color=auto'
+export DEFAULT_USER='arty'
+
+# Pure theme customization
+export PURE_PROMPT_SYMBOL=$
+
+# For add to $PATH from /etc/paths.d
+eval `/usr/libexec/path_helper -s`
