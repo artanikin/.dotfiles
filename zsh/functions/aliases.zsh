851456dc2446bb9877656bebca49bcf7f5de5a8d
Anikin Artyom <anikinartyom@gmail.com>
2017-11-12 23:52:47 +0500

    Move aliases

---
+++ zsh/functions/aliases.zsh
@@ -0,0 +1,18 @@
+alias cp="cp -iv"
+alias rm="rm -iv"
+alias mv="mv -iv"
+alias ls="ls -FGh"
+alias dotfiles="cd ~/.dotfiles"
+alias sed="sed -E"
+alias -g G="| grep "
+alias server="python -m SimpleHTTPServer 8000"
+alias tmux="tmux -2"
+alias v="vim"
+alias reload!="source ~/.zshrc"
+alias be="bundle exec"
+alias lc='colorls -r'
+alias rspec="clear; rspec"
+alias dm="docker-machine"
+alias mntnb="sshfs dev:/home/arty/projects/nb ~/mount/nb -ovolname=NB"
+alias mntcp="sshfs dev:/home/arty/projects/cp ~/mount/cp -ovolname=CP"
+
