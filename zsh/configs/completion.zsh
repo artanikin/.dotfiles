a6b08064d236652c99effe00851820e9ab330790
Anikin Artyom <anikinartyom@gmail.com>
2017-11-13 10:58:44 +0500

    ZSH menu autocompletion

--- zsh/configs/completion.zsh
+++ zsh/configs/completion.zsh
@@ -5,3 +5,5 @@ autoload -U compinit && compinit
 ## case-insensitive (all), partial-word and then substring completion
 zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' \
     'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+
+zstyle ':completion:*' menu select
