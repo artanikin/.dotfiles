00ce0efd6f93df79e5110c2063e53e03bfca7e2f
Anikin Artyom <anikinartyom@gmail.com>
2018-01-15 20:12:17 +0500

    Change default dir for screenshots
    Downloads directory use as single Inbox

--- os/mac_commands.sh
+++ os/mac_commands.sh
@@ -1,5 +1,8 @@
 # Скорость скрытия Dock панели
 defaults write com.apple.dock autohide-time-modifier -float 0.1; killall Dock
 
+# Сохранение скриншотов в папку Downloads
+defaults write com.apple.screencapture location ~/Downloads
+
 # Запуск приложения от неавторизоавнных пользователей
 sudo spctl --master-disable
