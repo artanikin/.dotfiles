1a0b0cd2591812047bf35e5eb52343f499f5d3a1
Anikin Artyom <anikinartyom@gmail.com>
2019-05-09 23:26:31 +0500

    Update vimode and pure prompt plugins

--- zsh/configs/general.zsh
+++ zsh/configs/general.zsh
@@ -3,9 +3,6 @@ export EDITOR=nvim
 export GREP_OPTIONS='--color=auto'
 export DEFAULT_USER='arty'
 
-# Pure theme customization
-export PURE_PROMPT_SYMBOL=$
-
 # For add to $PATH from /etc/paths.d
 eval `/usr/libexec/path_helper -s`
 
