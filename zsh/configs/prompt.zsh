548a5139026dbc49912a19b968f76ec0b3a64abf
Anikin Artyom <anikinartyom@gmail.com>
2019-08-01 00:32:40 +0500

    Change PURE prompt to POWERLEVEL10K

--- zsh/configs/prompt.zsh
+++ zsh/configs/prompt.zsh
@@ -1,10 +1,18 @@
 # PURE prompt
-prompt_newline='%666v'
+# prompt_newline='%666v'
 
-PROMPT=" $PROMPT"
-PURE_PROMPT_SYMBOL=' $'
-PURE_PROMPT_VICMD_SYMBOL=' ❮'
-PURE_GIT_DOWN_ARROW="⇣"
-PURE_GIT_UP_ARROW="⇡"
+# PROMPT=" $PROMPT"
+# PURE_PROMPT_SYMBOL=' $'
+# PURE_PROMPT_VICMD_SYMBOL=' ❮'
+# PURE_GIT_DOWN_ARROW="⇣"
+# PURE_GIT_UP_ARROW="⇡"
 
-zplug 'sindresorhus/pure', use:'pure.zsh', from:'github', as:'theme'
+# zplug 'sindresorhus/pure', use:'pure.zsh', from:'github', as:'theme'
+
+
+# POWERLEVEL10K prompt
+# https://github.com/romkatv/powerlevel10k
+zplug 'romkatv/powerlevel10k', use:'powerlevel10k.zsh-theme'
+
+# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+source ~/.p10k.zsh
