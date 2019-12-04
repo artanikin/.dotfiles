3b6f099cb59fb882b817f2858fd1acf73efe9cca
Anikin Artyom <anikinartyom@gmail.com>
2019-12-04 18:47:59 +0500

    Add completion for docker-compose

--- zsh/configs/completion.zsh
+++ zsh/configs/completion.zsh
@@ -1,6 +1,6 @@
 fpath=(~/.zsh/completion $fpath)
 
-autoload -U compinit && compinit
+autoload -Uz compinit && compinit -i
 
 ## case-insensitive (all), partial-word and then substring completion
 zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' \
