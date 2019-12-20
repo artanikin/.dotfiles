b40adbd3cfa3b6f5944717878bbcbb9433ce3c55
Anikin Artyom <anikinartyom@gmail.com>
2019-12-20 17:09:16 +0500

    Add bookmarks and plugins for nnn FM

--- zsh/configs/nnn.zsh
+++ zsh/configs/nnn.zsh
@@ -1,2 +1,3 @@
 export NNN_USE_EDITOR=1
-export NNN_BMS="h:~;d:~/Downloads;e:~/projects/eduson"
+export NNN_BMS="h:~;d:~/Downloads;e:~/projects/eduson;p:~/projects;n:/Users/artyomanikin/.yandex.disk/183688642/Yandex.Disk.localized/settings/notes"
+export NNN_PLUG='d:diffs;v:imgviu;t:treeview'
