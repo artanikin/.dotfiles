a4b14912589f0bdc6c87fc484097ae1f57cee444
Anikin Artyom <anikinartyom@gmail.com>
2020-04-19 00:37:24 +0500

    Some cleanup

--- zsh/configs/general.zsh
+++ zsh/configs/general.zsh
@@ -1,7 +1,7 @@
 export LANG=en_US.UTF-8
 export EDITOR=nvim
 export GREP_OPTIONS='--color=auto'
-export DEFAULT_USER='arty'
+export DEFAULT_USER='artyomanikin'
 
 # For add to $PATH from /etc/paths.d
 eval `/usr/libexec/path_helper -s`
