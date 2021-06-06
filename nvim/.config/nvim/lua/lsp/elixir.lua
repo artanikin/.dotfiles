918dbaf54cc5249460708ff6b1cef5552acc5179
Anikin Artyom <anikinartyom@gmail.com>
2021-06-06 21:28:06 +0500

    Update elixir-ls root dir

--- nvim/.config/nvim/lua/lsp/elixir.lua
+++ nvim/.config/nvim/lua/lsp/elixir.lua
@@ -1,6 +1,7 @@
 local elixir_ls_path = vim.fn.stdpath("data") .. "/lspinstall/elixir/elixir-ls"
 
 require'lspconfig'.elixirls.setup{
-	cmd={elixir_ls_path .. "/language_server.sh"};
+  cmd={elixir_ls_path .. "/language_server.sh"},
+  root_dir = require('lspconfig/util').root_pattern("mix.exs", ".git", ".")
 }
 
